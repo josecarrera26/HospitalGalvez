4ac85aa65d045a02663262af13000a4d7dd44d32
josecarrera26 <jose.lusicarrera199526@outlook.com>
2024-03-19 19:06:08 -0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	deleted:    Clase 3 - SO II.pdf
    	new file:   hospitalgalvez/.gitignore
    	new file:   hospitalgalvez/.mvn/wrapper/maven-wrapper.jar
    	new file:   hospitalgalvez/.mvn/wrapper/maven-wrapper.properties
    	new file:   hospitalgalvez/mvnw
    	new file:   hospitalgalvez/mvnw.cmd
    	new file:   hospitalgalvez/pom.xml
    	new file:   hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/HospitalgalvezApplication.java
    	new file:   hospitalgalvez/src/main/resources/application.properties
    	new file:   hospitalgalvez/src/test/java/com/umg/hospitalgalvez/hospitalgalvez/HospitalgalvezApplicationTests.java
    	deleted:    test.txt

---
+++ hospitalgalvez/src/test/java/com/umg/hospitalgalvez/hospitalgalvez/HospitalgalvezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.hospitalgalvez.hospitalgalvez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalgalvezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

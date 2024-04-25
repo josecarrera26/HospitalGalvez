41eec8019e14f8e8a283fa8f6f0a8707ac0d6827
josecarrera26 <jose.lusicarrera199526@outlook.com>
2024-04-24 20:41:39 -0600

    Todo publicado

--- hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/controller/CitaController.java
+++ hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/controller/CitaController.java
@@ -1,5 +1,26 @@
 package com.umg.hospitalgalvez.hospitalgalvez.controller;
 
+import com.umg.hospitalgalvez.hospitalgalvez.entity.Cita;
+import com.umg.hospitalgalvez.hospitalgalvez.services.CitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("citas")
 public class CitaController {
 
+    private final CitaService citaService;
+
+    @Autowired
+    public CitaController(CitaService citaService) {
+        this.citaService = citaService;
+    }
+
+    public ResponseEntity<List<Cita>> getAll(){
+        return ResponseEntity.ok(citaService.getAll());
+    }
 }
--- hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/controller/ExamenController.java
+++ hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/controller/ExamenController.java
@@ -1,5 +1,27 @@
 package com.umg.hospitalgalvez.hospitalgalvez.controller;
 
+import com.umg.hospitalgalvez.hospitalgalvez.entity.Examen;
+import com.umg.hospitalgalvez.hospitalgalvez.services.ExamenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("examenes")
 public class ExamenController {
+    private final ExamenService examenService;
+    @Autowired
+    public ExamenController(ExamenService examenService) {
+        this.examenService = examenService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Examen>> getList(){
+        return ResponseEntity.ok(examenService.getAll());
+    }
 
 }

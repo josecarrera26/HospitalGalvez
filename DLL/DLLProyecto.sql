b006e167a55e635f4cc8baeefe476432e63189cf
josecarrera26 <jose.lusicarrera199526@outlook.com>
2024-04-24 19:58:08 -0600

    dll actualizado

--- DLL/DLLProyecto.sql
+++ DLL/DLLProyecto.sql
@@ -1,1 +1,24 @@
-create database bendicion
+create database bendicion;
+
+CREATE SCHEMA hospital;
+
+--creacion de secuencia citas
+--drop sequence bendicion.hospital.examen_seq
+CREATE SEQUENCE bendicion.hospital.examen_seq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 0
+    maxvalue 999999;
+   
+--creacion de tabla citas
+--drop table examen;
+--select * from examen;
+create table examen (
+	id_examen int4 not null default nextval('bendicion.hospital.examen_seq') 
+		constraint pk_examen primary key,
+	descripcion varchar(250),
+	precio numeric(10,2),
+	observaciones varchar(250)
+);
+
+--insert into examen (descripcion, precio, observaciones) values ('prueba', 1234.20, 'observaciones')
--- hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/entity/Examen.java
+++ hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/entity/Examen.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "department")
+@Table(name = "examen")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

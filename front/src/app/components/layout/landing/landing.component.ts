749988c2bbe7e424ba6c32a4d6e5ee18174e7e53
Made <madissson27r@gmail.com>
2024-05-10 14:23:56 -0600

    Changes to be committed: 	modified:   front/src/app/app.routes.ts 	modified:   front/src/app/components/layout/header/header.component.html 	modified:   front/src/app/components/layout/landing/landing.component.html 	modified:   front/src/app/components/layout/landing/landing.component.ts 	modified:   front/src/styles.scss 	modified:   front/tailwind.config.js

--- front/src/app/components/layout/landing/landing.component.ts
+++ front/src/app/components/layout/landing/landing.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export default class  LandingComponent {
 
 }
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'poppins': ['Poppins'],
+      'tenor': ['Tenor Sans']
+    },
+    extend: {
+      colors: {
+        'text-landing': '#08376B'
+      },
+    },
   },
+  
   plugins: [
     require('flowbite/plugin')
   ],

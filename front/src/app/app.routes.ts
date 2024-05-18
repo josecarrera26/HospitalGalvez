dc310b1179e1efc46bc33801f4535cf59a711c60
Made <madissson27r@gmail.com>
2024-05-17 21:57:17 -0600

    Changes to be committed: 	modified:   front/src/app/app.routes.ts 	modified:   front/src/app/components/receta/receta.component.html 	modified:   front/src/index.html

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MenuadministradorComponent } from './components/menuprincipal/menuadministrador/menuadministrador.component';
+import { RecetaComponent } from './components/receta/receta.component';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,7 @@ export const routes: Routes = [
     },
     {path:'login', component:LoginComponent},
     {path:'menuadministrador', component:MenuadministradorComponent},
-
+    {path:'recetas', component:RecetaComponent},
     {path: '', redirectTo: 'index',pathMatch: 'full'},
     {path: '**',redirectTo: 'index',pathMatch: 'full'},
 ];

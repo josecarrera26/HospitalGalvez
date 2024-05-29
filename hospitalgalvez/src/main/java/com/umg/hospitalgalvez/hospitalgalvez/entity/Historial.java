2af6aad4c9d37dcbb6ec0a1080e90234ae0770db
aiaguilonp <aaguilonp1@miumg.edu.gt>
2024-05-29 15:42:47 -0600

    .

--- hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/entity/Historial.java
+++ hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/entity/Historial.java
@@ -1,7 +1,5 @@
 package com.umg.hospitalgalvez.hospitalgalvez.entity;
 
-import java.sql.Timestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;

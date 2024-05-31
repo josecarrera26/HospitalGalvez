ea1f1335b3e42f45dd0e8527f31b50fc4fc52805
FernandoChial <pchialv@miumg.edu.gt>
2024-05-30 23:31:01 -0600

    user

--- hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/repository/RecetaRepository.java
+++ hospitalgalvez/src/main/java/com/umg/hospitalgalvez/hospitalgalvez/repository/RecetaRepository.java
@@ -9,9 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RecetaRepository extends JpaRepository<Receta, Long> {
-    @Query("select r.id_receta, c.id_cita, r.fechaCreacion, p.nombre , m.nombre_medico " + 
-    " from Receta r join Cita c on c.id_cita = r.cita.id_cita join Medico m on m.id_medico  = c.id_medico.id_medico" + 
-    " join Paciente p on p.id_paciente = c.paciente.id_paciente where r.id_receta = :id_receta and m.id_medico = :id_medico ")
-       List<Object[]> findRecetasByMedico(@Param("id_receta")Long id_receta,@Param("id_medico")Long id_medico);
+    @Query("select r.id_receta, c.id_cita, r.fechaCreacion, p.nombre , m.nombre_medico " +
+            " from Receta r join Cita c on c.id_cita = r.cita.id_cita join Medico m on m.id_medico  = c.id_medico.id_medico"
+            +
+            " join Paciente p on p.id_paciente = c.paciente.id_paciente where r.id_receta = :id_receta and m.id_medico = :id_medico ")
+    List<Object[]> findRecetasByMedico(@Param("id_receta") Long id_receta, @Param("id_medico") Long id_medico);
 
 }
